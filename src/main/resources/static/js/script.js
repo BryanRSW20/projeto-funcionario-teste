867b6ef87c02b663857d79ef35e2c928615050a7
BryanRSW20 <bryanrsw20@outlook.com>
2022-06-30 09:44:48 -0300

    .

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,6 +13,27 @@ const deletar = function(){
     }
 }
 
+var modal = document.getElementById('Modal')
+
+var btn = document.getElementById('openModal')
+
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function(){
+    modal.style.display = 'block';
+}
+
+span.onclick = function(){
+    modal.style.display = 'none';
+
+}
+
+window.onclick = function(event){
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
 // const verificarCep = function(){
 //     let cep = $('#floatingInputCep').val().replace(/[^0-9]/,'')
 //     if (cep){
